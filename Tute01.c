8c66db5b860cfe37ef51befed202068a74ae3db1
IT21239748 <127530808+IT21239748@users.noreply.github.com>
2023-03-11 01:20:20 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,16 @@
 #include <stdio.h>
 
 int main() {
-  
-  return 0;
+   float n1, n2; float avg; float tot;        /*declaring two vaiables for entering the two marks and one vairable for finding average and another for finding
+                                                total.*/
+	printf("Enter mark 1 ");                  // First prompt for entering mark 1.
+	scanf("%f", &n1);                         // code to get the mark 1.
+	printf("Enter mark 2 ");                  // Second prompt for entering mark 2.
+	scanf("%f", &n2);                         // code to get the mark 2.
+	tot= n1+n2;                               // finding total in order to find average.
+	avg= tot/2;                               // finding average by using previously found total divided by the number of values entered by the user.
+	
+	printf("The average is %.1f", avg);       // printing the found average.
+   return 0;
 }
 
